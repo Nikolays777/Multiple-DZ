0d69774a51775019bd7cbbf86709859d3a57a307
Nikolays777 <dudchak_m@list.ru>
2020-09-15 17:53:31 +0300

    added search and advatages

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-
+  $('.search__btn-second').on('click', function() {
+    $('.search__btn-second').addClass('active')
+    $('.search__btn-first').removeClass('active')
+  })
+  $('.search__btn-first').on('click', function() {
+    $('.search__btn-first').addClass('active')
+    $('.search__btn-second').removeClass('active')
+  })
 
   
 });
